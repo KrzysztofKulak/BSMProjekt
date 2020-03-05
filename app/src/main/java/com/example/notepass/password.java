91f11d3c8f25ebf196e5129cb6f75a5beb627824
Krzysztof Kułak <krzysztofpiotrkulak@gmail.com>
2020-03-05 14:50:58 +0100

    add working passing note_text between activity and password view

---
+++ app/src/main/java/com/example/notepass/password.java
@@ -0,0 +1,152 @@
+package com.example.notepass;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+
+public class password extends AppCompatActivity {
+    private static final String ALGORITHM = "AES";
+    private static final byte[] keyValue =
+            new byte[]{'T', 'h', 'i', 's', 'I', 's', 'A', 'S', 'e', 'c', 'r', 'e', 't', 'K', 'e', 'y'};
+
+    public String encrypt(String valueToEnc) throws Exception {
+        Key key = generateKey();
+        Cipher c = Cipher.getInstance(ALGORITHM);
+        c.init(Cipher.ENCRYPT_MODE, key);
+        byte[] encValue = c.doFinal(valueToEnc.getBytes());
+        String encryptedValue = new String(encValue, StandardCharsets.UTF_8);
+        return encryptedValue;
+    }
+
+    public String decrypt(String encryptedValue) throws Exception {
+        Key key = generateKey();
+        Cipher c = Cipher.getInstance(ALGORITHM);
+        c.init(Cipher.DECRYPT_MODE, key);
+        byte[] decordedValue = encryptedValue.getBytes();
+        byte[] decValue = c.doFinal(decordedValue);
+        String decryptedValue = new String(decValue);
+        return decryptedValue;
+    }
+
+    private static Key generateKey() throws Exception {
+        Key key = new SecretKeySpec(keyValue, ALGORITHM);
+        return key;
+    }
+
+    EditText EditText1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Button login = findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText1 = (EditText) findViewById(R.id.password);
+                String pass = EditText1.getText().toString();
+                String encodedText = open("Note1.txt", pass);
+                if (encodedText == null) {
+                    encodedText = "";
+                }
+                String noteText = encodedText + EditText1.getText().toString();
+                Intent intent = new Intent(password.this, MainActivity.class);
+                intent.putExtra("NOTE_TEXT", noteText);
+                startActivityForResult(intent, 200);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        try {
+            super.onActivityResult(requestCode, resultCode, data);
+            if (requestCode == 200 && resultCode == RESULT_OK) {
+                String noteText = data.getStringExtra("NOTE_TEXT");
+                Toast.makeText(password.this, noteText,
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        } catch (Exception ex) {
+            Toast.makeText(password.this, ex.toString(),
+                    Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    public void save(String fileName) {
+        try {
+            OutputStreamWriter out =
+                    new OutputStreamWriter(openFileOutput(fileName, 0));
+            out.write(EditText1.getText().toString());
+            out.close();
+            Toast.makeText(this, "Note saved!", Toast.LENGTH_SHORT).show();
+        } catch (Throwable t) {
+            Toast.makeText(this, "Exception: " + t.toString(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    public String open(String fileName, String password) {
+        String content = "";
+        if (FileExists(fileName)) {
+            try {
+                InputStream in = openFileInput(fileName);
+                if (in != null) {
+                    InputStreamReader tmp = new InputStreamReader(in);
+                    BufferedReader reader = new BufferedReader(tmp);
+                    String str;
+                    StringBuilder buf = new StringBuilder();
+                    while ((str = reader.readLine()) != null) {
+                        buf.append(str + "\n");
+                    }
+                    in.close();
+                    content = buf.toString();
+                }
+            } catch (java.io.FileNotFoundException e) {
+            } catch (Throwable t) {
+                Toast.makeText(this, "Exception: " + t.toString(), Toast.LENGTH_LONG).show();
+            }
+        }
+        return content;
+    }
+
+
+    public boolean FileExists(String fname) {
+        File file = getBaseContext().getFileStreamPath(fname);
+        return file.exists();
+    }
+
+
+}
